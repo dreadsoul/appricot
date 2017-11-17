2980892c92284765d60db5a2137e3e6e6d2e934c
dreadsoul <dreadsoul@mail.ru>
2017-11-17 11:13:50 +0100

    time chaged into MA Deadline

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 // Countdown time
 
 // Set the date we're counting down to
-var countDownDate = new Date("dec 1, 2017 15:37:25").getTime();
+var countDownDate = new Date("apr 26, 2018 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
