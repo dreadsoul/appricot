8a1b31ae8cbb6a8b541ec39af982e9e6a4bed26c
dreadsoul <dreadsoul@mail.ru>
2018-08-24 13:09:53 +0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 // Countdown time
 
 // Set the date we're counting down to
-var countDownDate = new Date("apr 26, 2018 23:59:59").getTime();
+var countDownDate = new Date("sep 17, 2018 14:35:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
